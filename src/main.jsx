cb9bea4c0be035e8b108885f74f341b6fd5cb1fb
parham <parham.ilaghi@gmail.com>
2026-06-19 02:11:19 +0330

    do changes fro ghp

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { CartProvider } from './context/CartContext';
 import { ToastProvider } from './context/ToastContext';
@@ -8,12 +9,12 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ToastProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </ToastProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
